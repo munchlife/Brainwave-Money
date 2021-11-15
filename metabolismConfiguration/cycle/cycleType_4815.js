02e8793c9f27cd6a499a378639c5a7c0db477ad0
ALEXANDER THE MARC <marc@munchmode.com>
2021-11-15 10:45:28 -0500

    Add files via upload

---
+++ metabolismConfiguration/cycle/cycleType_4815.js
@@ -0,0 +1,173 @@
+'use strict';
+
+// cycleType_5814.js (model)
+
+// Local js modules
+var metabolism        = require('../../models/database');
+var Blockages     = require('../blockages');
+var CycleType = require('../../data/cycleTypes');
+
+module.exports = function(cellId, cycle) {
+
+    var validate = metabolism.Sequelize.Validator;
+
+    var self = this;
+    self.continueProcessing = true;
+    self.cellId         = cellId;
+    self.cycle              = cycle;
+
+    self.audit = function(messageNumber, message) {
+        return metabolism.CellGraph[self.cellId].CycleAudit
+            .create({
+                cycleId: self.cycle.cycleId,
+                messageNumber: messageNumber,
+                message: message
+            }, {transaction: null});
+    };
+
+    // -------------------------------------------------------------------------
+    self.psOpen = function() {
+        return self.audit(20000, ' (PS)Open - Start')
+            .then(function() {
+                if (validate.toInt(self.cycle.cellType) !== 5814)
+                    throw new Blockages.CycleProcessError(20101, 'ERROR: cellType invalid');
+
+                if (self.cycle.instanceId === null)
+                    throw new Blockages.CycleProcessError(20102, 'ERROR: instanceId not set');
+
+                if (self.cycle.deviceId === null)
+                    throw new Blockages.CycleProcessError(20103, 'ERROR: deviceId not set');
+
+                // TODO: consider bringing in later when multi-login support is in cell app
+                // if (self.cycle.staffCreatorId === null)
+                //     throw new Blockages.CycleProcessError(20104, 'ERROR: staffCreatorId not set');
+
+                // Cycle fields not checked here: staffDelivererId, originGeneId, deliveryMethod, status
+
+                if (self.cycle.distributedSignal !== CycleType.distributedSignalType.ENUM.SGL_USER.abbr)
+                    throw new Blockages.CycleProcessError(20105, 'ERROR: distributedSignal invalid');
+
+                // Cycle fields not checked here: taxPercentage, subTotal, chargeDiscount, chargeFee, chargeTax, chargeTip
+
+                if (!validate.isFloat(self.cycle.chargeTotal))
+                    throw new Blockages.CycleProcessError(20106, 'ERROR: chargeTotal invalid');
+
+                if (self.cycle.chargeTotal <= 0)
+                    throw new Blockages.CycleProcessError(20106, 'ERROR: chargeTotal invalid');
+
+                if (validate.toFloat(self.cycle.chargeTotal) !==
+                    validate.toFloat(self.cycle.chargeTotal.toFixed(2)))
+                    throw new Blockages.CycleProcessError(20106, 'ERROR: chargeTotal invalid');
+
+                // Cycle fields not checked here: cycleNotes
+
+                if (self.cycle.Sequences.length !== 0)
+                    throw new Blockages.CycleProcessError(20107, 'ERROR: attaching items to the cycle is not allowed');
+
+                self.continueProcessing = false;
+                return self.updateCycleStatus(CycleType.cycleStatusType.ENUM.RDYPRCS.status, 20002, ' (PS)Open - Updated Status');
+            })
+            .then(function() {
+                return self.audit(20001, ' (PS)Open - Finished');
+            });
+    };
+
+    self.psReadyForProcessing = function() {
+        return self.audit(30000, ' (PS)Ready for Processing - Start')
+            .then(function() {
+                return self.updateCycleStatus(CycleType.cycleStatusType.ENUM.PRCSNG.status, 30002, ' (PS)Ready for Processing - Updated Status');
+            })
+            .then(function() {
+                return self.audit(30001, ' (PS)Ready for Processing - Finished');
+            });
+    };
+
+    self.psProcessing = function() {
+        return self.audit(40000, ' (PS)Processing - Start')
+            .then(function() {
+                self.continueProcessing = false;
+
+                // Be sure the life is processed before completing the cycle
+                // if (self.cycle.Lifes.length !== 1 ||
+                //     self.cycle.Lifes[0].status !== CycleType.lifeStatusType.ENUM.COMPLT.status)
+                //     return metabolism.sequelize.Promise.resolve();
+                // else
+                    return self.updateCycleStatus(CycleType.cycleStatusType.ENUM.COMPLT.status, 40002, ' (PS)Processing - Updated Status');
+            })
+            .then(function() {
+                return self.audit(40001, ' (PS)Processing - Finished');
+            });
+    };
+
+    // -------------------------------------------------------------------------
+    self.process = function() {
+        self.continueProcessing = true;
+
+        return self.audit(10000, 'Cycle Processor - Start')
+            .then(function() {
+                return metabolism.sequelize.Promise.while(
+                    function() { return self.continueProcessing === true; },
+                    function() { return self.getCurrentPipelineSection(self.cycle.status)(); }
+                );
+            })
+            .catch(function(error) {
+                self.audit(error.code || 10101, 'Error process(): ' + error.message);
+
+                throw new Blockages.CycleProcessError(0, 'Error occurred during cycle processing; processing aborted.');
+            })
+            .finally(function() {
+                self.audit(10001, 'Cycle Processor - Finished');
+            });
+    };
+
+    // -------------------------------------------------------------------------
+    self.getCurrentPipelineSection = function(status) {
+        var section;
+
+        switch (status) {
+            case CycleType.cycleStatusType.ENUM.OPEN.status: // Open
+                section = self.psOpen;
+                break;
+
+            case CycleType.cycleStatusType.ENUM.RDYPRCS.status: // Ready for Processing
+                section = self.psReadyForProcessing;
+                break;
+
+            case CycleType.cycleStatusType.ENUM.PRCSNG.status: // Processing
+                section = self.psProcessing;
+                break;
+
+            case CycleType.cycleStatusType.ENUM.COMPLT.status: // Complete
+                throw new Error('Cycle already marked as COMPLETE');
+
+            case CycleType.cycleStatusType.ENUM.CNCLLD.status: // Cancelled
+                throw new Error('Cannot process cycle marked as CANCELLED');
+
+            default: // UNKNOWN STATUS
+                throw new Error('Unknown status in cycle');
+        }
+
+        return section;
+    };
+
+    // -------------------------------------------------------------------------
+    self.updateCycleStatus = function(status, auditNumber, auditMessage) {
+        switch (status) {
+            case CycleType.cycleStatusType.ENUM.OPEN.status: // Open
+            case CycleType.cycleStatusType.ENUM.RDYPRCS.status: // Ready for Processing
+            case CycleType.cycleStatusType.ENUM.PRCSNG.status: // Processing
+            case CycleType.cycleStatusType.ENUM.COMPLT.status: // Complete
+            case CycleType.cycleStatusType.ENUM.CNCLLD.status: // Cancelled
+                self.cycle.status = status;
+                break;
+
+            default: // UNKNOWN STATUS
+                throw new Error('Cell cycle type \'4815\' does not support this status');
+        }
+
+        return self.cycle.save({ fields: ['status'] })
+                .then(function() {
+                    return self.audit(auditNumber, auditMessage);
+                });
+    };
+};
---
+++ metabolismConfiguration/cycle/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// index.js (cycle)
+
+var Cycles = module.exports = {};
+
+Cycles['4815'] = require('./cycleType_4815');

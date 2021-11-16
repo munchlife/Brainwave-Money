c39a9ca1e8897caeb1ee66c928b78ca0f08a0399
ALEXANDER THE MARC <marc@munchmode.com>
2021-11-15 19:19:20 -0500

    Update cell.js

--- metabolismRouter/cell.js
+++ metabolismRouter/cell.js
@@ -13,13 +13,13 @@ var mv      = require('mv');
 
 // Local js modules
 var Middlewares  = require('./middlewares');
-var metabolism   = require('../../models/database');
-var CellGraph   = require('../../config/cellGraph');
-var Immunities   = require('../../config/immunities');
-var Genes        = require('../../config/genes');
-var Blockages    = require('../../config/blockages');
-var CountryCodes = require('../../data/countryCodes');
-var GeneType     = require('../../data/geneTypes');
+var metabolism   = require('../../metabolismLifeModels/database');
+var CellGraph   = require('../../metabolismConfiguration/cellGraph');
+var Immunities   = require('../../metabolismConfiguration/immunities');
+var Genes        = require('../../metabolismConfiguration/genes');
+var Blockages    = require('../../metabolismConfiguration/blockages');
+var CountryCodes = require('../../metabolismTypes/countryCodes');
+var GeneType     = require('../../metabolismTypes/geneTypes');
 
 var validate = metabolism.Sequelize.Validator;
 

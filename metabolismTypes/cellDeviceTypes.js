55cf0b9f7c27f441717dea2881efead2fae6264a
ALEXANDER THE MARC <marc@munchmode.com>
2022-01-27 12:43:37 -0500

    Update cellDeviceTypes.js

--- metabolismTypes/cellDeviceTypes.js
+++ metabolismTypes/cellDeviceTypes.js
@@ -1,20 +1,20 @@
 'use strict';
 
-// merchantDeviceTypes.js
+// cellDeviceTypes.js
 
-var merchantDeviceTypes = module.exports = {};
+var cellDeviceTypes = module.exports = {};
 
-merchantDeviceTypes.ENUM = {
-    IOS: { abbr: 'IOS', fullName: 'iOS Device' },
-
+cellDeviceTypes.ENUM = {
+       ELF: { abbr: 'ELF', fullName: 'Extremely Low-Frequency Device' },
+    // IOS: { abbr: 'IOS', fullName: 'iOS Device' },
     // AND: { abbr: 'AND', fullName: 'Android Device' },
     // WIN: { abbr: 'WIN', fullName: 'Windows Device' },
     // BBY: { abbr: 'BBY', fullName: 'BlackBerry Device' }
 };
 
-merchantDeviceTypes.totalCount = 0;
-merchantDeviceTypes.abbrs      = [];
-for (var merchantDevice in merchantDeviceTypes.ENUM) {
-    merchantDeviceTypes.totalCount++;
-    merchantDeviceTypes.abbrs.push(merchantDeviceTypes.ENUM[merchantDevice].abbr);
+cellDeviceTypes.totalCount = 0;
+cellDeviceTypes.abbrs      = [];
+for (var cellDevice in cellDeviceTypes.ENUM) {
+    cellDeviceTypes.totalCount++;
+    cellDeviceTypes.abbrs.push(cellDeviceTypes.ENUM[cellDevice].abbr);
 }

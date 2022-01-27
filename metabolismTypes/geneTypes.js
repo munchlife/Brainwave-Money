660d6affe97bbb8ede85b3cfa46957db14147b75
ALEXANDER THE MARC <marc@munchmode.com>
2022-01-27 12:39:43 -0500

    Update geneTypes.js

--- metabolismTypes/geneTypes.js
+++ metabolismTypes/geneTypes.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// serviceTypes.js
+// geneTypes.js
 
-var serviceTypes = module.exports = {};
+var geneTypes = module.exports = {};
 
 serviceTypes.ENUM = {
-    ALL:     { value: 0,      text: 'all'     },
-    PAYMENT: { value: 1 << 0, text: 'payment' },
-    LOYALTY: { value: 1 << 1, text: 'loyalty' },
-    CHECKIN: { value: 1 << 2, text: 'checkin' }
+    ALL:       { value: 0,      text: 'all'     },
+    SIGNALING: { value: 1 << 0, text: 'signaling' },
+ // LOYALTY:   { value: 1 << 1, text: 'loyalty' },
+ // CHECKIN:   { value: 1 << 2, text: 'checkin' }
 };

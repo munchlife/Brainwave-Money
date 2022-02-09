c2693ee045660444433c37174c9522f46a547da7
ALEXANDER THE MARC <marc@munchmode.com>
2022-02-09 13:08:57 -0500

    Update dictionary.js

--- metabolismConfiguration/genes/dictionary.js
+++ metabolismConfiguration/genes/dictionary.js
@@ -6,8 +6,8 @@
 var qs = require('querystring');
 
 // Dependency packages
-var debug   = require('debug')('munch:config:gene:1006');
-var verbose = require('debug')('munch:verbose:config:gene:1006');
+var debug   = require('debug')('munch:config:gene:dictionary');
+var verbose = require('debug')('munch:verbose:config:gene:dictionary');
 var rest    = require('restling');
 
 // Local js modules

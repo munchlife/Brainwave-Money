77735566208f7e9560cc12c812f50e4d8a53b043
ALEXANDER THE MARC <marc@munchmode.com>
2022-02-15 11:44:34 -0500

    Create lifeSpecies.js

---
+++ metabolismTypes/lifeSpecies.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// lifeSpecies.js
+
+// SOURCE: https://en.wikipedia.org/wiki/Category:Lists_of_species
+
+// lsNumber    = LSN
+// description = Life Species
+
+var LifeSpecies = module.exports = [
+  { lsNumber: 1, mccMin: null, mccMax: null, abbr: 'XXXXXX', description: 'Humanoid' },
+  { lsNumber: 2, mccMin: null, mccMax: null, abbr: 'XXXXXX', description: 'Cow' },
+  { lsNumber: 3, mccMin: null, mccMax: null, abbr: 'XXXXXX', description: 'Trimeresurus Gramineus' },
+  { lsNumber: 4, mccMin: null, mccMax: null, abbr: 'XXXXXX', description: 'Saccharomyces Cerevisiae' },
+];

d39e51f02577c6575ad0f25038cce75e0cc1dbbc
ALEXANDER THE MARC <marc@munchmode.com>
2023-12-29 13:48:21 -0500

    Update and rename geneStakeholder.js to serviceStakeholder.js

--- metabolismLifeModels/serviceStakeholder.js
+++ metabolismLifeModels/serviceStakeholder.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// geneStakeholder.js (model)
+// serviceStakeholder.js (model)
 
 var STAKEHOLDER_PERMISSIONS_MAX = 256;
 
 module.exports = function(sequelize, DataTypes) {
-    var GeneStakeholder = sequelize.define('GeneStakeholder', {
+    var ServiceStakeholder = sequelize.define('ServiceStakeholder', {
         stakeholderId: {
             type: DataTypes.BIGINT.UNSIGNED,
             primaryKey: true,
@@ -16,15 +16,15 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false,
             validate: {
                 isInt: {
-                    msg: 'Gene stakeholder immunities value must be an integer'
+                    msg: 'Service stakeholder immunities value must be an integer'
                 },
                 min: {
                     args: 0,
-                    msg: 'Gene stakeholder immunities value must be a positive number'
+                    msg: 'Service stakeholder immunities value must be a positive number'
                 },
                 max: {
                     args: STAKEHOLDER_PERMISSIONS_MAX,
-                    msg: 'Gene stakeholder immunities value must be less than or equal to ' + STAKEHOLDER_PERMISSIONS_MAX
+                    msg: 'Service stakeholder immunities value must be less than or equal to ' + STAKEHOLDER_PERMISSIONS_MAX
                 }
             }
         }
@@ -34,19 +34,19 @@ module.exports = function(sequelize, DataTypes) {
         // updatedAt:  true,
         paranoid: true,            // adds deletedAt timestamp (won't actually delete entries)
         // freezeTableName: true,  // defaulted globally
-        tableName: 'geneStakeholder', // force table name to this value
+        tableName: 'serviceStakeholder', // force table name to this value
         validate: {
         },
         classMethods: {
             associate: function(models) {
-                GeneStakeholder.belongsTo(models.Life, {               foreignKey: 'lifeId' });
-                GeneStakeholder.belongsTo(models.Gene, {               foreignKey: 'geneId' });
-                GeneStakeholder.hasMany(models.Token,  { as: 'Tokens', foreignKey: 'geneStakeholderId' });
+                ServiceStakeholder.belongsTo(models.Life, {               foreignKey: 'lifeId' });
+                ServiceStakeholder.belongsTo(models.Service, {               foreignKey: 'serviceId' });
+                ServiceStakeholder.hasMany(models.Token,  { as: 'Tokens', foreignKey: 'serviceStakeholderId' });
             }
         },
         instanceMethods: {
         }
     });
 
-    return GeneStakeholder;
+    return ServiceStakeholder;
 };

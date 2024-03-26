307897d849530a4f2a64b5c2da7ce3f733a3a1f0
ALEXANDER THE MARC <marc@munchmode.com>
2024-03-26 09:53:30 -0400

    Update brainwaveCategoryCodes.js

--- metabolismTypes/brainwaveCategoryCodes.js
+++ metabolismTypes/brainwaveCategoryCodes.js
@@ -1,17 +1,17 @@
 'use strict';
 
-// cellCategoryCodes.js
+// brainwaveCategoryCodes.js
 
 // SOURCE: https://en.wikipedia.org/wiki/Neural_oscillation
 
-// cccNumber   = CCC
-// description = Cell Category
+// bccNumber   = BCC
+// description = Brainwave Type
 // reportable  = Reportable under 6041/6041A and Authority for Exception
 
 var CellCategoryCodes = module.exports = [
- { cccNumber: 0742, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Delta',                                                          reportable: true  },
- { cccNumber: 0763, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Theta',                                                        reportable: true  },
- { cccNumber: 0780, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Alpha',                                                          reportable: true  },
- { cccNumber: 1520, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Beta',                                                         reportable: true  },
- { cccNumber: 1711, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Gamma',                                                           reportable: true  },
+ { bccNumber: 0742, bccMin: null, bccMax: null, abbr: 'XXXXXX', description: 'Delta',                                                          reportable: true  },
+ { bccNumber: 0763, bccMin: null, bccMax: null, abbr: 'XXXXXX', description: 'Theta',                                                        reportable: true  },
+ { bccNumber: 0780, bccMin: null, bccMax: null, abbr: 'XXXXXX', description: 'Alpha',                                                          reportable: true  },
+ { bccNumber: 1520, bccMin: null, bccMax: null, abbr: 'XXXXXX', description: 'Beta',                                                         reportable: true  },
+ { bccNumber: 1711, bccMin: null, bccMax: null, abbr: 'XXXXXX', description: 'Gamma',                                                           reportable: true  },
 ];

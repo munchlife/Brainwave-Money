ee5e524ac8e489059650addbea51172a17350fc6
ALEXANDER THE MARC <marc@munchmode.com>
2024-03-26 09:52:56 -0400

    Update and rename cellCategoryCodes.js to brainwaveCategoryCodes.js

---
+++ metabolismTypes/brainwaveCategoryCodes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// cellCategoryCodes.js
+
+// SOURCE: https://en.wikipedia.org/wiki/Neural_oscillation
+
+// cccNumber   = CCC
+// description = Cell Category
+// reportable  = Reportable under 6041/6041A and Authority for Exception
+
+var CellCategoryCodes = module.exports = [
+ { cccNumber: 0742, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Delta',                                                          reportable: true  },
+ { cccNumber: 0763, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Theta',                                                        reportable: true  },
+ { cccNumber: 0780, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Alpha',                                                          reportable: true  },
+ { cccNumber: 1520, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Beta',                                                         reportable: true  },
+ { cccNumber: 1711, cccMin: null, cccMax: null, abbr: 'XXXXXX', description: 'Gamma',                                                           reportable: true  },
+];
